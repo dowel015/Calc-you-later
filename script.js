ddf10c5869fd7098f695ea2ee52eb2a6ea432e5c
dowel015 <dowel015@umn.edu>
2023-02-02 21:32:22 -0600

    add operate function that evaluates expressions

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function divide(a,b) {
     return a/b;
 }
 
+function operate(a,b,c) {
+    if(a=="+") {
+        return add(b,c);
+    }
+    if(a=="-") {
+        return subtract(b,c);
+    }
+    if(a=="*") {
+        return multiply(b,c);
+    }
+    if(a=="/") {
+        return divide(b,c);
+    }
+}
+let a=5, b=10;
+
+let c = operate("+",a,b);
+let d = operate("-",a,b);
+let e = operate("*",a,b);
+let f = operate("/",a,b);
+
+console.log(a,b,c,d,e,f);
